7eb4dee8117345318787b2bfb1f45abee39ba89d
Ana maria Martin Garcia <anamary9_bcn@hotmail.com>
2015-10-02 18:51:34 +0200

    First commet.

---
+++ practica3/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.sumaBtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RestaBtn = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'sumaBtn
+        '
+        Me.sumaBtn.Location = New System.Drawing.Point(53, 99)
+        Me.sumaBtn.Name = "sumaBtn"
+        Me.sumaBtn.Size = New System.Drawing.Size(75, 23)
+        Me.sumaBtn.TabIndex = 0
+        Me.sumaBtn.Text = "Suma"
+        Me.sumaBtn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(3, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(68, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.TextBox2.Location = New System.Drawing.Point(130, 49)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(60, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Mistral", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(260, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(129, 44)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "resultado"
+        '
+        'RestaBtn
+        '
+        Me.RestaBtn.Location = New System.Drawing.Point(53, 214)
+        Me.RestaBtn.Name = "RestaBtn"
+        Me.RestaBtn.Size = New System.Drawing.Size(75, 23)
+        Me.RestaBtn.TabIndex = 5
+        Me.RestaBtn.Text = "resta"
+        Me.RestaBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(875, 320)
+        Me.Controls.Add(Me.RestaBtn)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.sumaBtn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents sumaBtn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RestaBtn As Button
+End Class
---
+++ practica3/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Dim op1 As Integer = 0
+    Dim op2 As Integer = 0
+    Dim resultado As Integer
+
+
+    Private Sub sumaBtn_Click(sender As Object, e As EventArgs) Handles sumaBtn.Click
+
+
+        op1 = CInt(TextBox1.Text)
+        op2 = CInt(TextBox2.Text)
+
+        resultado = op1 + op2
+
+        Label1.Text = resultado.ToString()
+
+    End Sub
+
+    Private Sub RestaBtn_Click(sender As Object, e As EventArgs) Handles RestaBtn.Click
+        op1 = CInt(TextBox1.Text)
+        op2 = CInt(TextBox2.Text)
+
+        resultado = op1 - op2
+
+        Label1.Text = resultado.ToString()
+    End Sub
+End Class
